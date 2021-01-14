ef9a1cdbd4e70b235a6405c9e7f3794decc03858
Jesper <jesperfriberg94@hotmail.com>
2021-01-14 10:56:54 +0100

    all done

---
+++ src/Inlämningsuppgift/Category.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Jesper Friberg Spång
+ * Date: 2020-12-24
+ * Time: 14:51
+ * Project: Inlämningsuppgift
+ * Copyright: MIT
+ */
+package Inlämningsuppgift;
+
+
+public enum Category {
+    FOOD, HISTORY, CHEMISTRY
+
+}
